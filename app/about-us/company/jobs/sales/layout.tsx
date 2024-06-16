fe61a072d473c573bdec50c457c3c1df51a16e92
ooyuo <doyounjeong0@gmail.com>
2024-06-16 22:20:48 +0900

    feat: routing

---
+++ app/about-us/company/jobs/sales/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "JobSales",
+};
+
+export default function SalesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}$$$$$</div>;
+}
---
+++ components/navigation.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const path = usePathname();
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "🚄" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About</Link> {path === "/about-us" ? "🚢" : ""}
+        </li>
+      </ul>
+    </nav>
+  );
+}
